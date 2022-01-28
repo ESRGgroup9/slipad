ac0746946195b9b43bb06747d20f36dba5b498ad
Tomas Abreu <tomasld.abreu@gmail.com>
2022-01-28 11:34:13 +0000

    rc application; add signin, signup

--- remotesystem/inc/RCApplication.h
+++ remotesystem/inc/RCApplication.h
@@ -16,13 +16,51 @@ public:
 	RCApplication(int sd, MYSQL* database);
 
 private:
-	// Callbacks for Application command list
-	// Add new lamppost
-	// Modify lamppost
-	// Consult Lamppost Network
+// Callbacks for Application command list
+/**
+  * @brief  Add new lamppost and its associated region, location and parking_space
+  * @param 
+  * @return 
+  *
+  * Detailed description
+  */
 	static int addCb(int, char *[]);
+	
+/**
+  * @brief  Modify lamppost
+  * @param 
+  * @return 
+  *
+  * Detailed description
+  */
 	static int modifyCb(int, char *[]);
+
+/**
+ * @brief Consult Lamppost Network
+ * @param 
+ * @return 
+ *
+ * Detailed description
+ */
 	static int consultCb(int, char *[]);
+
+/**
+ * @brief Check sign in operator credentials
+ * @param 
+ * @return 
+ *
+ * Detailed description
+ */
+	static int signInCb(int, char *[]);
+
+/**
+ * @brief Sign up new operator
+ * @param 
+ * @return 
+ *
+ * Detailed description
+ */
+	static int signUpCb(int, char *[]);
 
 private:
 	static RCApplication* thisPtr;
